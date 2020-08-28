2bbfe8d7b20d5b394d81e753968ff7d4b368e31a
robertosc <robert.s.c30@gmail.com>
2020-08-28 11:37:16 -0600

    cli commands

--- data_extraction.py
+++ data_extraction.py
@@ -26,6 +26,7 @@ Usage:
 """
 import sys
 import yaml
+import csv
 import glob
 from collections import OrderedDict
 import argparse
@@ -75,8 +76,11 @@ def multiples_archivos(path, num_params=0):
 	compounds = {} #Se almacenan datos en diccionario
 	if(num_params == 0):
 		num_params = numInput() #Se pide el numero de datos a extraer
-	
-	files = glob.glob(path+"*.log", recursive=True)
+	try:
+		files = glob.glob(path+"*.log", recursive=True)
+	except:
+		print("No .log files found")
+		return
 	#print(files)
 	for k in range(len(files)):
 		try:
@@ -110,46 +114,54 @@ def represent_dictionary_order(self, dict_data):
 def setup_yaml():
 	yaml.add_representer(OrderedDict, represent_dictionary_order)
 		
-def yaml_format(comp_dict, filename): #Se genera una función para pasar el diccionario a formato .yaml
-	setup_yaml()
-	"""
-	if(filename == None):
-		option = str(input("Do you want a specific namefile? [y/n] "))
-		if(option == "n"):
-			print("The file will be saved under the name compounds.yaml")
-			filename = "compounds.yaml"
-		else:
-			filename = str(input("Enter the name of the file: "))
-	"""
+def humanreadable_format(comp_dict, filename, format): #Function to write the dictionary in yaml or csv format
+	while(format == None or format != "yaml" and format != "csv"):
+		format = str(input("In what format do you want to save you data? [yaml/csv] "))
+
 	if (filename == None):
 		filename = "compounds"
 	punto = filename.find(".") #Se busca un punto para evitar errores de nombre
+	
 	if(punto != -1):
-		filename = filename[0:punto]+".yaml"
+		filename = filename[0:punto]+"."+str(format)
 		print("File will be saved under the name: %s" % filename)
 	else:
-		filename = filename+".yaml"
+		filename = filename+"."+str(format)
 		print("File will be saved under the name: %s" % filename)
-	
-	file = open(filename, 'w')
-		
-	yaml.dump(comp_dict, file)
-	file.close() 
+
+	f = open(filename, 'w')
+	if(format == "yaml"):
+		setup_yaml()
+		yaml.dump(comp_dict, f)
+	else:
+		w = csv.writer(f)
+		w.writerow(comp_dict.keys())
+		w.writerow(comp_dict.values())
+	f.close()
+
+
+
+
 
 def main():
-	my_parser = argparse.ArgumentParser()
+	my_parser = argparse.ArgumentParser(description="This is a script for the extraction and sorting of affinity parameters of the docking results from pyMOL with .log format\
+		\n Usage example: python3 data_extraction.py -i /home/user/logfiles/ -f yaml -o compounds -n 3\n\n Usage example extracts 3 data points from file found at /home/user/logfiles/\
+		,saves them as compunds.yaml")
 	my_parser.add_argument('-n', action='store', type=int, help="Number of parameters to extract")
 	my_parser.add_argument('-f', action='store', type=str, help="Format of the savings file [yaml/csv]")
 	my_parser.add_argument('-o', action='store', type=str, help="Name of the file where data will be saved")
-	my_parser.add_argument('-i', action='store', type=str, help="Path to the .log format files")
-	args = my_parser.parse_args()
+	#my_parser.add_argument('-i', action='store', type=str, help="Path to the .log format files")
 	
+	requiredNamed = my_parser.add_argument_group('required named arguments')
+	requiredNamed.add_argument('-i', action='store', type=str, help='Path to the .log format files', required=True)
+	
+	args = my_parser.parse_args()
+
 	if(args.n == None):
 		sorted_compounds = multiples_archivos(args.i)
 	else:
 		sorted_compounds = multiples_archivos(args.i, args.n)
 	
-	if(args.f == None):
-		yaml_format(sorted_compounds, args.o)
+	humanreadable_format(sorted_compounds, args.o, args.f)
 
 main()
